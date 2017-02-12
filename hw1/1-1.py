41e37642e5c028bfc8758b65d79aca8e9f85733d
Jaivardhan Kapoor bt ee <jkapoor@ccpc187.cc.iitk.ac.in>
2017-02-12 23:18:50 +0530

    Finalise Homework 1, add PDF

--- hw1/1-1.py
+++ hw1/1-1.py
@@ -5,22 +5,23 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 x = np.linspace(-8, 8, 300)
-eta = np.linspace(1,15,15)
+eta = 3
 
 def func(X, eta):
 	return np.divide(np.exp(np.divide(-np.power(X,2), 2*eta)), np.sqrt(eta*2*np.pi))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-X, Y = np.meshgrid(x, eta)
-zs = np.array([func(x,eta) for x,eta in zip(np.ravel(X), np.ravel(Y))])
-Z = zs.reshape(X.shape)
-ax.plot_surface(X, Y, Z)
+#ax = fig.add_subplot(111, projection='3d')
 
+#X, Y = np.meshgrid(x, eta)
+#zs = np.array([func(x,eta) for x,eta in zip(np.ravel(X), np.ravel(Y))])
+#Z = zs.reshape(X.shape)
+#ax.plot_surface(X, Y, Z)
 
-ax.set_xlabel('$x$')
-ax.set_ylabel('$\\eta$')
-ax.set_zlabel('$p(x|\\eta)$')
+y = func(x,eta)
+plt.plot(x,y)
+plt.xlabel('$x$')
+#ax.set_ylabel('$\\eta$')
+plt.ylabel('$p(x|\\eta=3)$')
 plt.savefig('1-1.png')
 
